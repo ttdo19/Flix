21c590504e901856d6ef0c886f67626d33cc2ca0
ttdo19 <thuytrang19072001@gmail.com>
2022-09-11 17:27:32 -0400

    my first commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Trang Do on 9/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
